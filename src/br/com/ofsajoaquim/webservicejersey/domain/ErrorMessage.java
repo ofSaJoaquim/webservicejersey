55d1ce02c7834296971efc5d29b197b777816279
luiz.sa.joaquim@gmail.com <luiz.sa.joaquim@gmail.com>
2021-05-25 22:43:34 -0300

    Terminando a primeira parte
    	* Adicionado classes de controle de Exception
    	* Adicionado padrão de messagens de erros
    	* Adicionado filtro

---
+++ src/br/com/ofsajoaquim/webservicejersey/domain/ErrorMessage.java
@@ -0,0 +1,30 @@
+package br.com.ofsajoaquim.webservicejersey.domain;
+
+public class ErrorMessage {
+	private String message;
+	private int code;
+	
+	public ErrorMessage() { }
+		
+	public ErrorMessage(String message, int code) {
+		super();
+		this.message = message;
+		this.code = code;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	public void setCode(int code) {
+		this.code = code;
+	}
+}
