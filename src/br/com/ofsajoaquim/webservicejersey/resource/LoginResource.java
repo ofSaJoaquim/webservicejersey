cab270baa27d350293265dfdac8baf00f474456e
luiz.sa.joaquim@gmail.com <luiz.sa.joaquim@gmail.com>
2021-05-28 13:26:55 -0300

    Adicionado gerador de token

---
+++ src/br/com/ofsajoaquim/webservicejersey/resource/LoginResource.java
@@ -0,0 +1,63 @@
+
+ 
+package br.com.ofsajoaquim.webservicejersey.resource;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Link;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+
+import br.com.ofsajoaquim.webservicejersey.dto.LoginDTO;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Path("/login")
+public class LoginResource {
+    private final SecretKey CHAVE = Keys.hmacShaKeyFor("7f-j&amp;CKk=coNzZc0y7_4obMP?#TfcYq%fcD0mDpenW2nc!lfGoZ|d?f&amp;RNbDHUX6fdsfsberewr34564565675".getBytes(StandardCharsets.UTF_8));
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response post(LoginDTO usuario)
+    {
+        try{
+            if(usuario.getLogin().equals("teste") && usuario.getSenha().equals("1234"))
+            {
+                String jwtToken = Jwts.builder()
+                    .setSubject(usuario.getLogin())
+                    .setIssuer("localhost:9090")
+                    .setIssuedAt(new Date())
+                    .setExpiration(Date.from(LocalDateTime.now().plusMinutes(15L).atZone(ZoneId.systemDefault()).toInstant()))
+                    .signWith(CHAVE, SignatureAlgorithm.HS512)
+                    .compact();
+
+                Link link = Link.fromUriBuilder(
+                                    UriBuilder.fromUri("http://localhost:8080/")
+                                    .path("pessoa")
+                                )
+                                .rel("lista_pessoas")
+                                .type("GET")
+                                .build();
+                
+                return Response.status(Response.Status.OK).entity(jwtToken).links(link).build();
+            }
+            else
+                return Response.status(Response.Status.UNAUTHORIZED).entity("Usuário e/ou senha inválidos").build();
+        }
+        catch(Exception ex)
+        {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+        } 
+    }
+}
--- src/br/com/ofsajoaquim/webservicejersey/service/UsuarioService.java
+++ src/br/com/ofsajoaquim/webservicejersey/service/UsuarioService.java
@@ -4,6 +4,7 @@ import javax.persistence.NoResultException;
 
 import br.com.ofsajoaquim.webservicejersey.dao.UsuarioDAO;
 import br.com.ofsajoaquim.webservicejersey.domain.Usuario;
+import br.com.ofsajoaquim.webservicejersey.dto.LoginDTO;
 
 public class UsuarioService {
 	private final UsuarioDAO usuarioDAO = new UsuarioDAO();
@@ -15,5 +16,12 @@ public class UsuarioService {
 	      return false;
 	   }
 	   return true;
+	}
+	
+	public boolean validarUsuario(LoginDTO login) {
+		Usuario usuario = new Usuario();
+		usuario.setUsername(login.getLogin());
+		usuario.setPassword(login.getSenha());
+		return this.validarUsuario(usuario);
 	}
 }
